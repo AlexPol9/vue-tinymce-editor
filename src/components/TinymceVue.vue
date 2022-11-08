b39368e9855da0b5c1fa21e64aa921936efcbb74
DIGDES\Polikarpov.Al <polikarpov.al@digdes.com>
2022-11-08 23:09:29 +0400

    fix: add tinymce dynamic import

--- src/components/TinymceVue.vue
+++ src/components/TinymceVue.vue
@@ -5,59 +5,7 @@
 </template>
 
 <script>
-   // Import TinyMCE
-    import tinymce from 'tinymce/tinymce';
 
-    // A theme is also required
-    import 'tinymce/themes/modern/theme';
-
-    // Any plugins you want to use has to be imported
-    import 'tinymce/plugins/advlist';
-    import 'tinymce/plugins/wordcount';
-    import 'tinymce/plugins/autolink';
-    import 'tinymce/plugins/autosave';
-    import 'tinymce/plugins/charmap';
-    import 'tinymce/plugins/codesample';
-    import 'tinymce/plugins/contextmenu';
-    import 'tinymce/plugins/emoticons';
-    import 'tinymce/plugins/fullscreen';
-    import 'tinymce/plugins/hr';
-    import 'tinymce/plugins/imagetools';
-    import 'tinymce/plugins/insertdatetime';
-    import 'tinymce/plugins/link';
-    import 'tinymce/plugins/media';
-    import 'tinymce/plugins/noneditable';
-    import 'tinymce/plugins/paste';
-    import 'tinymce/plugins/print';
-    import 'tinymce/plugins/searchreplace';
-    import 'tinymce/plugins/tabfocus';
-    import 'tinymce/plugins/template';
-    import 'tinymce/plugins/textpattern';
-    import 'tinymce/plugins/visualblocks';
-    import 'tinymce/plugins/anchor';
-    import 'tinymce/plugins/autoresize';
-    import 'tinymce/plugins/bbcode';
-    import 'tinymce/plugins/code';
-    import 'tinymce/plugins/colorpicker';
-    import 'tinymce/plugins/directionality';
-    import 'tinymce/plugins/fullpage';
-    import 'tinymce/plugins/help';
-    import 'tinymce/plugins/image';
-    import 'tinymce/plugins/importcss';
-    import 'tinymce/plugins/legacyoutput';
-    import 'tinymce/plugins/lists';
-    import 'tinymce/plugins/nonbreaking';
-    import 'tinymce/plugins/pagebreak';
-    import 'tinymce/plugins/preview';
-    import 'tinymce/plugins/save';
-    import 'tinymce/plugins/spellchecker';
-    import 'tinymce/plugins/table';
-    import 'tinymce/plugins/textcolor';
-    import 'tinymce/plugins/toc';
-    import 'tinymce/plugins/visualchars';
-    
-    import 'tinymce/skins/lightgray/skin.min.css'
-   
     export default {
         name: 'tinymce',
         props: { 
@@ -84,15 +32,16 @@
         data(){
             return {
                 content : '',
-                editor : null,
-                cTinyMce : null,
                 checkerTimeout: null,
                 isTyping : false
             }; 
         },
         mounted(){
             this.content = this.value;
-            this.init();  
+            this.importTinyMCE()
+            .then(() => {
+                this.init();  
+            })
         },
         beforeDestroy () {
             this.editor.destroy();
@@ -114,6 +63,13 @@
                 }
             }
         },
+        computed: {
+            editor() {
+                return window.tinymce.getInstanceById ? 
+                    window.tinymce.getInstanceById(this.id) : 
+                    (window.tinymce.get ? window.tinymce.get(this.id) : window.tinymce.editors[this.id])
+            }
+        },
         methods: {
             init(){
                 let options = {
@@ -124,16 +80,15 @@
                     plugins: this.plugins,
                     init_instance_callback : this.initEditor
                 };
-                tinymce.init(this.concatAssciativeArrays(options, this.other_options));
+                window.tinymce.init(this.concatAssciativeArrays(options, this.other_options));
             },
             initEditor(editor) {
-                this.editor = editor;
                 editor.on('KeyUp', (e) => {
-                    this.submitNewContent();
+                    this.submitNewContent(editor);
                 });
                 editor.on('Change', (e) => {
-                    if(this.editor.getContent() !== this.value){
-                        this.submitNewContent();
+                    if(editor.getContent() !== this.value){
+                        this.submitNewContent(editor);
                     }
                     this.$emit('editorChange', e);
                 });
@@ -142,9 +97,9 @@
                     this.$emit('input', this.content);
                 });
                 if(this.readonly){
-                    this.editor.setMode('readonly');
+                    editor.setMode('readonly');
                 } else {
-                    this.editor.setMode('design');
+                    editor.setMode('design');
                 }
 
                 this.$emit('editorInit', editor);
@@ -157,7 +112,7 @@
                 for ( let key in array2) dest[key] = array2[key];
                 return dest;
             },
-            submitNewContent(){
+            submitNewContent(editor){
                 this.isTyping = true;
                 if(this.checkerTimeout !== null)
                     clearTimeout(this.checkerTimeout);
@@ -165,13 +120,79 @@
                         this.isTyping = false;
                     }, 300);
 
-                this.$emit('input', this.editor.getContent());
+                this.$emit('input', editor.getContent());
+            },
+
+            importTinyMCE() {
+                if (!window.tinymce) {
+                    return import('tinymce/tinymce')
+                        .then(() => {
+                            const promises = [];
+
+                            promises.push(import('tinymce/themes/modern/theme'));
+
+                            promises.push(import('tinymce/plugins/advlist'));
+                            promises.push(import('tinymce/plugins/wordcount'));
+                            promises.push(import('tinymce/plugins/autolink'));
+                            promises.push(import('tinymce/plugins/autosave'));
+                            promises.push(import('tinymce/plugins/charmap'));
+                            promises.push(import('tinymce/plugins/codesample'));
+                            promises.push(import('tinymce/plugins/contextmenu'));
+                            promises.push(import('tinymce/plugins/emoticons'));
+                            promises.push(import('tinymce/plugins/fullscreen'));
+                            promises.push(import('tinymce/plugins/hr'));
+                            promises.push(import('tinymce/plugins/imagetools'));
+                            promises.push(import('tinymce/plugins/insertdatetime'));
+                            promises.push(import('tinymce/plugins/link'));
+                            promises.push(import('tinymce/plugins/media'));
+                            promises.push(import('tinymce/plugins/noneditable'));
+                            promises.push(import('tinymce/plugins/paste'));
+                            promises.push(import('tinymce/plugins/print'));
+                            promises.push(import('tinymce/plugins/searchreplace'));
+                            promises.push(import('tinymce/plugins/tabfocus'));
+                            promises.push(import('tinymce/plugins/template'));
+                            promises.push(import('tinymce/plugins/textpattern'));
+                            promises.push(import('tinymce/plugins/visualblocks'));
+                            promises.push(import('tinymce/plugins/anchor'));
+                            promises.push(import('tinymce/plugins/autoresize'));
+                            promises.push(import('tinymce/plugins/bbcode'));
+                            promises.push(import('tinymce/plugins/code'));
+                            promises.push(import('tinymce/plugins/colorpicker'));
+                            promises.push(import('tinymce/plugins/directionality'));
+                            promises.push(import('tinymce/plugins/fullpage'));
+                            promises.push(import('tinymce/plugins/help'));
+                            promises.push(import('tinymce/plugins/image'));
+                            promises.push(import('tinymce/plugins/importcss'));
+                            promises.push(import('tinymce/plugins/legacyoutput'));
+                            promises.push(import('tinymce/plugins/lists'));
+                            promises.push(import('tinymce/plugins/nonbreaking'));
+                            promises.push(import('tinymce/plugins/pagebreak'));
+                            promises.push(import('tinymce/plugins/preview'));
+                            promises.push(import('tinymce/plugins/save'));
+                            promises.push(import('tinymce/plugins/spellchecker'));
+                            promises.push(import('tinymce/plugins/table'));
+                            promises.push(import('tinymce/plugins/textcolor'));
+                            promises.push(import('tinymce/plugins/toc'));
+                            promises.push(import('tinymce/plugins/visualchars'));
+                            
+                            promises.push(import('tinymce/skins/lightgray/skin.min.css'));
+
+                            return Promise.all(promises)
+                        })
+                        .catch((e) => {
+                            console.log('Error!', e)
+                        })
+                } else {
+                    return new Promise((resolve) => {
+                        resolve()
+                    })
+                }
             }
         }
     }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
 
 </style>
